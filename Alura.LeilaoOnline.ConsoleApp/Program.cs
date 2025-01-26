5d9396c10b2fb5c6f61b3479bf455b952661b1f0
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-01-26 11:54:17 -0300

    :sparkles: feat: Implementação de testes unitários, utilizando um método próprio para validação

--- Alura.LeilaoOnline.ConsoleApp/Program.cs
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -5,8 +5,25 @@ namespace Alura.LeilaoOnline.ConsoleApp
 {
     class Program
     {
-        static void Main()
+        private static void Verifica(double esperado, double obtido)
         {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"TESTE FALHOU!\nEsperado: {esperado}\nObtido: {obtido}");
+            }
+            Console.ForegroundColor = cor;
+        }
+        
+        private static void LeilaoComVariosLances()
+        {
+            //Arranje - cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
             var maria = new Interessada("Maria", leilao);
@@ -16,9 +33,38 @@ namespace Alura.LeilaoOnline.ConsoleApp
             leilao.RecebeLance(fulano, 1000);
             leilao.RecebeLance(maria, 990);
 
+            //Act - método sob teste
             leilao.TerminaPregao();
 
-            Console.WriteLine(leilao.Ganhador.Valor);
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void LeilaoComApenasUmLance()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            
+            leilao.RecebeLance(fulano, 800);
+                                    
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
         }
     }
 }
