d9321a139c6c3bdafbfd84bcb9d95c058eec2eea
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-01-28 21:49:11 -0300

    :white_check_mark: test: Teste de Leilão sem lances

--- Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -6,6 +6,52 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTestes
     {
         [Fact]
+        public void LeilaoComTresClientes()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var pedro = new Interessada("Pedro", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var joao = new Interessada("João", leilao);
+
+            leilao.RecebeLance(joao, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(joao, 990);
+            leilao.RecebeLance(pedro, 1400);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+            
+            //Assert
+            var valorEsperado = 1400;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(pedro, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(fulano, 1000);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+            
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+        
+        [Fact]
         public void LeilaoComVariosLances()
         {
             //Arranje - cenário
@@ -44,6 +90,22 @@ namespace Alura.LeilaoOnline.Tests
             var valorEsperado = 800;
             var valorObtido = leilao.Ganhador.Valor;
 
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoSemLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            
+            //Act - método sob teste
+            leilao.TerminaPregao();
+            
+            //Assert
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+            
             Assert.Equal(valorEsperado, valorObtido);
         }
     }
