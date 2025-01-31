930c88cc8577c33d14af9d2d52fde11bbeceea25
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-01-30 23:19:58 -0300

    :white_check_mark: test: Testes de ofertas no leilão - LeilaoRecebeOferta.cs

---
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,32 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeOferta
+    {
+        [Theory]
+        [InlineData(2, new double[] { 800, 900 })]
+        [InlineData(4, new double[] { 1000, 1200, 1400, 1300 })]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado(int qtdeEsperada, double[] ofertas)
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+            leilao.TerminaPregao();
+
+            //Act - método sob teste
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert
+            var qtdeObtida = leilao.Lances.Count();
+            Assert.Equal(qtdeEsperada, qtdeObtida);
+        }
+    }
+}
